eff64cb0cb35fd0bbce4ad8e167b513469cdde10
hamedghaderi <hamedghaderii@gmail.com>
2014-03-11 10:30:01 +0330

    fix hide problem when mouse is over the details box

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
 var btns = document.getElementsByClassName('get-info');
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('mouseover', showDetails, false);
-    btns[i].addEventListener('mouseout', hiddenDisplay, false);
+    btns[i].addEventListener('mouseout', hiddenDetails, false);
+    btns[i].nextElementSibling.addEventListener('mouseover', showParagraph, false);
+    btns[i].nextElementSibling.addEventListener('mouseout', hideParagraph, false);
 }
 
 function showDetails() {
     this.nextElementSibling.style.display = "inline-block";
 }
 
-function hiddenDisplay() {
+function hiddenDetails() {
     this.nextElementSibling.style.display = "none";
+
+}
+
+function showParagraph () {
+    this.style.display = "inline-block";
+}
+
+function hideParagraph () {
+    this.style.display = "none";
 }
