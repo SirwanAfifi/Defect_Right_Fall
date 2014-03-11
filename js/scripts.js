156c337b811d7a0c7544c8c8ce2407f60e5f7890
hamedghaderi <hamedghaderii@gmail.com>
2014-03-11 10:05:58 +0330

    Just Organize

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 var btns = document.getElementsByClassName('get-info');
 for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('mouseover', function (e) {
-        //console.log(this.innerText);
-        //console.log(e.target.nextSibling.nextSibling);
-        e.target.nextSibling.nextSibling.style.display = "inline";
-    }, false);
-    btns[i].addEventListener('mouseout', function (e) {
-        //console.log(this.innerText);
-        //console.log(e.target.nextSibling.nextSibling);
-        e.target.nextSibling.nextSibling.style.display = "none";
-    }, false);
+    btns[i].addEventListener('mouseover', showDetails, false);
+    btns[i].addEventListener('mouseout', hiddenDisplay, false);
+}
+
+function showDetails() {
+    this.nextElementSibling.style.display = "inline-block";
+}
+
+function hiddenDisplay() {
+    this.nextElementSibling.style.display = "none";
 }
